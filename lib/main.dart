b8de0baf08e63151ddfabdee66f64d1f09829b0f
triyono777 <yono.tri>
2021-02-24 19:32:46 +0700

    no state restoration

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  RootRestorationScope( // Register a restoration scope for the entire app!
-    restorationId: 'root',
-    child: MyApp(),
-  ),
-);
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -21,19 +16,16 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-// Our state should be mixed-in with RestorationMixin
-class _HomePageState extends State<HomePage> with RestorationMixin {
-
-  // For each state, we need to use a restorable proper ty
-  final RestorableInt _index = RestorableInt(0);
+class _HomePageState extends State<HomePage> {
+  int _index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Index is ${_index.value}')),
+      body: Center(child: Text('Index is ${_index}')),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _index.value,
-        onTap: (i) => setState(() => _index.value = i),
+        currentIndex: _index,
+        onTap: (i) => setState(() => _index = i),
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
               icon: Icon(Icons.home),
@@ -50,17 +42,5 @@ class _HomePageState extends State<HomePage> with RestorationMixin {
         ],
       ),
     );
-  }
-
-  @override
-  // The restoration bucket id for this page,
-  // let's give it the name of our page!
-  String get restorationId => 'home_page';
-
-  @override
-  void restoreState(RestorationBucket oldBucket, bool initialRestore) {
-    // Register our property to be saved every time it changes,
-    // and to be restored every time our app is killed by the OS!
-    registerForRestoration(_index, 'nav_bar_index');
   }
 }
